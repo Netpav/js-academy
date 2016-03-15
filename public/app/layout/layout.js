a5991039472ef7d0692b9f49e675cc5f6791ec4b
Pavel Netolický <pavel.netolicky@gmail.com>
2016-03-15 21:56:37 +0100

    Comments layout

--- public/app/layout/layout.js
+++ public/app/layout/layout.js
@@ -6,7 +6,7 @@ var app = angular.module('sample-app');
 
 app.config(function($stateProvider){
   $stateProvider.state('layout',{
-    //pouziti abstrakce --- znovupouziti
+    //pouziti abstract --- znovupouziti
     // nesmi byt zaroven url a abstract
     abstract:true,
     templateUrl:'app/layout/layout.html'
